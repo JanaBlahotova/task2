ec579acf69bf5c0f6e90e1e711409761767bba75
Jana Blahotová <jana.blahotova@email.cz>
2024-01-29 14:19:16 +0100

    ukol 2

--- src/com/engeto/ja/Carrots.java
+++ src/com/engeto/ja/Carrots.java
@@ -74,7 +74,5 @@ public class Carrots {
         this.carConsumption = carConsumption;
     }
 
-    public String setIpAddress(int i, int i1, int i2, int i3) {
-        return ipAddress;
-    }
+
 }
